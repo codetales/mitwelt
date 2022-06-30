d1a26cdf86dc216206ece35382b3f9ddad4b1b96
Julian Fahrer <julian@fahrer.net>
2022-06-30 13:37:33 +0200

    Provide access to env vars

--- lib/mitwelt.rb
+++ lib/mitwelt.rb
@@ -1,8 +1,50 @@
 # frozen_string_literal: true
 
+require "dry-types"
 require_relative "mitwelt/version"
 
-module Mitwelt
+class Mitwelt
   class Error < StandardError; end
-  # Your code goes here...
+
+  module Types
+    include Dry.Types()
+  end
+
+  def self.fetch(key, type: :string, default: nil, required: false)
+    new(key, type: type, default: default, required: required).fetch
+  end
+
+  def initialize(key, type:, default:, required:)
+    @key = key
+    @type = type
+    @default = default.freeze
+    @required = required
+  end
+
+  def fetch
+    value = parsed_value
+    raise(Error.new("#{@key} is required but not in ENV")) if @required && value.nil?
+    value
+  end
+
+  def parsed_value
+    parser[value]
+  rescue Dry::Types::CoercionError => error
+    raise Error.new "Unable to parse #{@key} from ENV as #{@type}: #{error.message}"
+  end
+
+  def parser
+    {
+      string: Types::Coercible::String,
+      symbol: Types::Coercible::Symbol,
+      integer: Types::Params::Integer,
+      boolean: Types::Params::Bool,
+      timestamp: Types::Params::Time,
+      date: Types::Params::Date
+    }[@type].optional
+  end
+
+  def value
+    ENV[@key].nil? ? @default : ENV[@key]
+  end
 end
--- spec/mitwelt_spec.rb
+++ spec/mitwelt_spec.rb
@@ -5,7 +5,99 @@ RSpec.describe Mitwelt do
     expect(Mitwelt::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe ".fetch" do
+    def set_env(key, value)
+      allow(ENV).to receive(:[]).and_call_original
+      allow(ENV).to receive(:[]).with(key) { value }
+    end
+
+    context "for timestamps" do
+      def timestamp
+        "2020-01-01T00:00:00Z"
+      end
+
+      it "parses the timestamp" do
+        set_env("time", timestamp)
+        expect(described_class.fetch("time", type: :timestamp)).to eq(Time.parse(timestamp))
+      end
+
+      context "if the ENV var is not set" do
+        context "if required" do
+          it "raises an error" do
+            expect { described_class.fetch("time", type: :timestamp, required: true) }.to raise_error(described_class::Error)
+          end
+
+          context "with a default" do
+            it "returns the default" do
+              time = Time.now
+              expect(described_class.fetch("time", type: :timestamp, required: true, default: time)).to eq(time)
+            end
+          end
+
+          context "if the default is of the wrong type" do
+            it "raises an error" do
+              time = "foobar"
+              expect { described_class.fetch("time", type: :timestamp, required: true, default: time) }.to raise_error(described_class::Error)
+            end
+          end
+        end
+
+        context "if not required" do
+          it "returns nil" do
+            expect(described_class.fetch("time", type: :timestamp)).to be_nil
+          end
+        end
+      end
+    end
+
+    context "for booleans" do
+      it "parses true" do
+        set_env("foo", "true")
+        expect(described_class.fetch("foo", type: :boolean)).to eq(true)
+      end
+
+      it "parses false" do
+        set_env("foo", "false")
+        expect(described_class.fetch("foo", type: :boolean)).to eq(false)
+      end
+
+      it "parses 1 as true" do
+        set_env("foo", "1")
+        expect(described_class.fetch("foo", type: :boolean)).to eq(true)
+      end
+
+      it "parses 0 as false" do
+        set_env("foo", "0")
+        expect(described_class.fetch("foo", type: :boolean)).to eq(false)
+      end
+
+      context "if the ENV var is not set" do
+        context "if required" do
+          it "raises an error" do
+            expect { described_class.fetch("time", type: :timestamp, required: true) }.to raise_error(described_class::Error)
+          end
+
+          context "with a default" do
+            it "returns the default" do
+              time = Time.now
+              expect(described_class.fetch("time", type: :timestamp, required: true, default: time)).to eq(time)
+            end
+          end
+
+          context "if the default is of the wrong type" do
+            it "raises an error" do
+              time = "foobar"
+              expect { described_class.fetch("time", type: :timestamp, required: true, default: time) }.to raise_error(described_class::Error)
+            end
+          end
+        end
+
+        context "if not required" do
+          it "returns nil" do
+            expect(described_class.fetch("time", type: :timestamp)).to be_nil
+          end
+        end
+      end
+    end
   end
 end
