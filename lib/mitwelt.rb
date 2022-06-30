b2999ad1424a709453ed74c59c9aa93d6f8f6534
Julian Fahrer <julian@fahrer.net>
2022-06-30 13:31:43 +0200

    Add gem skeleton

---
+++ lib/mitwelt.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "mitwelt/version"
+
+module Mitwelt
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/mitwelt_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Mitwelt do
+  it "has a version number" do
+    expect(Mitwelt::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
